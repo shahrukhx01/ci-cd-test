c0a91f116a08661e679bae41b960d2999fc1de2c
shahrukhx01 <sk28671@gmail.com>
2020-05-14 16:13:21 +0500

    add udagram

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=FPnnWQvg7DDluPyDpy6s
+export POSTGRES_HOST=udagram.copj1fvau2as.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-f70c69d8-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=jahdsaskjd
+export URL=http://localhost:8100
